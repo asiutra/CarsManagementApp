db56f10073b7ca5e03e0ed1117db024488c771c7
asiutra <adamsiutra@onet.pl>
2021-01-20 18:40:38 +0100

    Implement user action and view + simple consume API

--- CarsManagementApp/CarsManagementApp/Controllers/UserController.cs
+++ CarsManagementApp/CarsManagementApp/Controllers/UserController.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using CarsManagementApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarsManagementApp.Controllers
 {
     public class UserController : Controller
     {
         [HttpGet]
-        public IActionResult ShowUser()
+        public async Task<IActionResult> ShowUser()
         {
-            return View();
+            List<User> userList = new List<User>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:5000/api/user"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    userList = JsonConvert.DeserializeObject<List<User>>(apiResponse);
+                }
+            }
+
+            return View(userList);
         }
     }
 }
